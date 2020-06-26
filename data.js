0011cc9ec1010508a325c6a9e9b5d0ca51ec9d99
ismarads <ismarpp.santos@gmail.com>
2020-06-25 22:22:04 -0300

    desafio proposto durante o módulo de início ao back end no bootcamp Launchbase da Rocketseat

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports =[
+{
+    id:"starter",
+    title: "Starter",
+    discription:"3 módulos<br> 100% gratuito"
+
+},
+  
+{
+    id:"launchbase",
+    title: "Domine programação do zero",
+    discription:"Um bootcamp 100% online seja um programador full-stack"
+
+},
+  
+{
+    id:"gostack",
+    title: "Descenvolvimento web e mobile",
+    discription:"Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile"
+
+}
+  
+
+
+
+]
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server= express()
+const courses = require("./data")
+
+server.use(express.static('public'))
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+
+})
+
+nunjucks.configure("views",{
+
+    express:server
+})
+
+server.get("/", function(req,res){
+    return res.render("about")
+
+})
+
+server.get("/courses", function(req,res){
+    return res.render("courses")
+
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function(){
+
+    console.log("rodando de boa")
+})
